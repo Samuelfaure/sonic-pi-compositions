3660ae11d7083019e83e8ff1516467a337cda1cf
Samuel FAURE <samuel.faure.dev@gmail.com>
2021-05-02 17:00:06 +0200

    Update raining-on-ritalin.rb

--- raining-on-ritalin.rb
+++ raining-on-ritalin.rb
@@ -4,19 +4,19 @@ bpm = 120.to_s
 use_bpm bpm.to_i
 
 live_loop :atmos do
-  play_sample_timed atmos, bpm, 0
+  sample_with_sleep atmos, bpm, 0
 end
 
 live_loop :beat do
-  play_sample_timed beats, bpm, 0
+  sample_with_sleep beats, bpm, 0
 end
 
 live_loop :hihat do
-  play_sample_timed beats, "OldHats", 0, amp: 0.5
+  sample_with_sleep beats, "OldHats", 0, amp: 0.5
 end
 
 live_loop :chords do
-  play_sample_timed chords, bpm, 1, amp: 1.5
+  sample_with_sleep chords, bpm, 1, amp: 1.5
 end
 
 live_loop :paf do
